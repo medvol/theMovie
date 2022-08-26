81fee0fd680f2e5626409d05ce7800d1e1537f21
Sergey Shishkin <Shishkin_SU@ukr.net>
2022-08-26 15:48:21 +0300

    add created list

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { MovieApiService } from './js/api-movie-service';
 import { createCategoryList } from './js/sidebar-category';
 import { createMarkupMovies } from './js/create-markup-movies';
 
-import './js/api-movie-service';
-import './js/modal-close-btn';
-import './js/modal-info-open';
+// import './js/api-movie-service';
+// import './js/modal-close-btn';
+// import './js/modal-info-open';
 
 // import './js/pagination';
 
@@ -14,14 +14,12 @@ const refs = {
   categoryList: document.querySelector('[data-list ="render"]'),
   mainContainer: document.querySelector('.main-container'),
   videos: document.querySelector('.videos'),
-
-}
+};
 
 let mainPage = true;
 
 if (!mainPage) {
   refs.mainContainer.classList.remove('videos');
-  
 }
 
 window.addEventListener('resize', function () {
@@ -32,32 +30,39 @@ window.addEventListener('resize', function () {
   }
 });
 
-
-
-addEventListener('DOMContentLoaded', loadSidebarCategory, { once: true });
+addEventListener('DOMContentLoaded', loadSidebarCategory, {
+  once: true,
+});
 
 const categoryMovie = new MovieApiService();
 
-async function loadSidebarCategory() {  
+async function loadSidebarCategory() {
   const categoryMovieList = await categoryMovie.fetchGenresDescription();
+  // console.log(categoryMovieList);
   createCategoryList(categoryMovieList, refs.categoryList);
+}
 
+addEventListener('DOMContentLoaded', loadList);
+
+const newsWeekApiService = new MovieApiService();
+
+async function loadList() {
+  const categoryWeekList = await newsWeekApiService.fetchTrendWeekMovie();
+  console.log(categoryWeekList);
+  createMarkupMovies(categoryWeekList, refs.videos);
 }
 
 refs.categoryList.addEventListener('click', onClickCategory);
 
 async function onClickCategory(event) {
-  const element = event.target.closest("li[data-id]");
+  const element = event.target.closest('li[data-id]');
   const id = element.dataset.id;
   const ganres = await categoryMovie.fetchMoviesForGenres(id);
   // mainPage = false;
   // refs.mainContainer.classList.add('videos')
   // refs.mainContainer.innerHTML = '';
-  refs.videos.innerHTML = ''
-  createMarkupMovies(ganres, refs.videos)
+  refs.videos.innerHTML = '';
+  createMarkupMovies(ganres, refs.videos);
 
-  console.log(ganres)
+  console.log(ganres);
 }
-
-
-
