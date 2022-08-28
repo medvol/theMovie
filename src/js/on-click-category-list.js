61c6d9daf11e85bd0e29ac3e7c6c2c437e9a500c
Sergey Shishkin <Shishkin_SU@ukr.net>
2022-08-28 12:55:21 +0300

    add-preload-category

--- src/js/on-click-category-list.js
+++ src/js/on-click-category-list.js
@@ -1,5 +1,6 @@
 import { MovieApiService } from './api-movie-service';
 import { createMarkupMovies } from './create-markup-movies';
+import { slickLoader } from './loader';
 
 const films = document.querySelector('.main-films');
 const pageTitle = document.querySelector('.main-header');
@@ -11,12 +12,18 @@ const categoryMovie = new MovieApiService();
 export default async function onClickCategory(event) {
   const element = event.target.closest('li[data-id]');
   const id = element.dataset.id;
-  const ganres = await categoryMovie.fetchMoviesForGenres(id);
-  films.innerHTML = '';
   videos.innerHTML = '';
+  films.innerHTML = '';
   pageTitle.textContent = element.firstElementChild.textContent;
-
   pageSubTitle.classList.add('visually-hidden');
-  createMarkupMovies(ganres, videos);
 
+  slickLoader();
+  try {
+    const ganres = await categoryMovie.fetchMoviesForGenres(id);
+
+    createMarkupMovies(ganres, videos);
+    SlickLoader.disable();
+  } catch (error) {
+    console.log(error);
+  }
 }
