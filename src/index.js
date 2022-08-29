734c62b887d626aaf780b4a53a75793bf0e31374
Marry Tymofiichuk <masha432alex@i.ua>
2022-08-29 23:06:27 +0300

    change modal

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './sass/index.scss';
 import {
   onOpenModal,
   onCloseModal,
-  // onBackdropClick,
 } from './js/modal-our-team';
 
 import {
@@ -44,9 +43,9 @@ const refs = {
   discover: document.querySelector('[data-name="discover"]'),
   trending: document.querySelector('[data-name="trending"]'),
   playlist: document.querySelector('[data-name="playlist"]'),
-  openModal: document.querySelector('#js-team-modal'),
-  closeModalBtn: document.querySelector('[data-modal-close]'),
-  backdrop: document.querySelector('.js-backdrop'),
+  openModal: document.querySelector('[data-action="open-modal"]'),
+  closeModalBtn: document.querySelector('[data-action="data-modal-close"]'),
+  backdrop: document.querySelector('[data-modal]'),
 
   pageSubTitle: document.querySelector('.most-watched'),
   searchBar: document.querySelector('.search-bar'),
@@ -68,7 +67,6 @@ window.addEventListener('resize', function () {
 });
 
 refs.modalCloseBtn.addEventListener('click', onModalCloseBtn);
-
 refs.openModal.addEventListener('click', onOpenModal);
 refs.closeModalBtn.addEventListener('click', onCloseModal);
 refs.backdrop.addEventListener('click', onBackdropClick);
@@ -101,19 +99,21 @@ refs.searchBar.addEventListener(
 
 refs.mainContainer.addEventListener('click', onModalShowInfoCard);
 
-let splide = new Splide('.splide', {
+
+
+var splide = new Splide('.splide', {
   perPage: 3,
-  gap: '2rem',
+  gap: '1rem',
   breakpoints: {
     640: {
       perPage: 2,
-      gap: '.7rem',
+      gap: '.2rem',
       height: '6rem',
     },
     480: {
       perPage: 1,
-      gap: '.7rem',
-      height: '12rem',
+      gap: '.2rem',
+      height: '6rem',
     },
   },
 });
--- src/js/modal-our-team.js
+++ src/js/modal-our-team.js
@@ -1,21 +1,22 @@
-const onBackdrop = document.querySelector('.js-backdrop')
+const onBackdrop = document.querySelector('[data-modal]')
+const footerBtnText = document.querySelector('.footer-btn-text')
 
 function onOpenModal() {
   window.addEventListener('keydown', closeModalByEsc);
   onBackdrop.classList.remove('is-hiddene');
+  footerBtnText.classList.toggle('footer-btn-text-click')
 }
 
+
 function onCloseModal() {
+  footerBtnText.classList.toggle('footer-btn-text-click')
   onBackdrop.classList.add('is-hiddene');
 }
 
-function onBackdropClick(e) {
-  if (e.currentTarget === e.target) onCloseModal();
-}
 
 function closeModalByEsc(e) {
   if (e.code === 'Escape') {
     onCloseModal();
   }
 }
-export { onOpenModal, onCloseModal, closeModalByEsc, onBackdropClick };
+export { onOpenModal, onCloseModal, closeModalByEsc };
