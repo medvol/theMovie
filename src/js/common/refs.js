bf35092c5df9bde82437bc626f3d13c4848abe56
Sergey Shishkin <95885361+Stavr101@users.noreply.github.com>
2022-08-31 12:35:37 +0300

    Update refs.js

--- src/js/common/refs.js
+++ src/js/common/refs.js
@@ -1,5 +1,4 @@
-
-export default  refs = {
+export const refs = {
   categoryList: document.querySelector('[data-list ="render"]'),
   mainContainer: document.querySelector('.main-container'),
   videos: document.querySelector('.videos'),
@@ -21,4 +20,32 @@ export default  refs = {
   modalCloseBtn: document.querySelector('.close-btn-card'),
 paginationContainer: document.querySelector('#pagination'),
   footer: document.querySelector('.footer')
-};
+}
+
+
+
+
+
+// export default  refs = {
+//   categoryList: document.querySelector('[data-list ="render"]'),
+//   mainContainer: document.querySelector('.main-container'),
+//   videos: document.querySelector('.videos'),
+//   sidebar: document.querySelector('.sidebar'),
+//   films: document.querySelector('.main-films'),
+//   pageTitle: document.querySelector('.main-header'),
+//   logo: document.querySelector('.logo-expand'),
+//   discover: document.querySelector('[data-name="discover"]'),
+//   trending: document.querySelector('[data-name="trending"]'),
+//   playlist: document.querySelector('[data-name="playlist"]'),
+//   openModal: document.querySelector('[data-action="open-modal"]'),
+//   closeModalBtn: document.querySelector('[data-action="data-modal-close"]'),
+//   backdrop: document.querySelector('[data-modal]'),
+//   pageSubTitle: document.querySelector('.most-watched'),
+//   searchBar: document.querySelector('.search-bar'),
+//   overlay: document.querySelector('.overlay'),
+//   modalCardMovie: document.querySelector('.modal_movie_card'),
+//   pageSubTitle: document.querySelector('.most-watched'),
+//   modalCloseBtn: document.querySelector('.close-btn-card'),
+// paginationContainer: document.querySelector('#pagination'),
+//   footer: document.querySelector('.footer')
+// };
